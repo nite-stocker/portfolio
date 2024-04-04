88ac545886f0ce91627224b1564f87ebd676acec
Ryan Mason <86732851+nite-stocker@users.noreply.github.com>
2024-04-03 21:57:53 -0500

    First commit after move from VS Code to Pycharm. Add gitignore, format with Black, fix readme typo.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Portfolio website
 
-This Flask web app is the first step towards showcasing my coding projects. The app uses the Univers portfolio template found at [free-css.com](https://www.free-css.com/free-css-templates/page270/univers).
+This Flask web app is the first step towards showcasing my coding projects. The app uses the Univers portfolio template 
+found at [free-css.com](https://www.free-css.com/free-css-templates/page270/univers).
 
 The portfolio app is hosted at [ry4n.pythonanywhere.com](https://ry4n.pythonanywhere.com).
--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, request, redirect
 import csv
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/<string:page_name>")
 def html_page(page_name):
     return render_template(page_name)
+
 
 # def write_to_database(data):
 #     with open("database.txt", mode = "a") as database:
@@ -18,13 +21,17 @@ def html_page(page_name):
 #         message = data["message"]
 #         file = database.write(f"\n{email},{subject},{message}")
 
+
 def write_to_csv(data):
-    with open("database.csv", newline = '', mode = "a") as database_csv:
+    with open("database.csv", newline="", mode="a") as database_csv:
         email = data["email"]
         subject = data["subject"]
         message = data["message"]
-        csv_writer = csv.writer(database_csv, delimiter = ',', quotechar = '"', quoting = csv.QUOTE_ALL)
+        csv_writer = csv.writer(
+            database_csv, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
+        )
         csv_writer.writerow([email, subject, message])
+
 
 @app.route("/submit_form", methods=["POST", "GET"])
 def submit_form():
@@ -34,6 +41,6 @@ def submit_form():
             write_to_csv(data)
             return redirect("thankyou.html")
         except:
-            return "Didn't save to database."    
+            return "Didn't save to database."
     else:
         return "Something went wrong."
